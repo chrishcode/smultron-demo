03781e70fb76e1645ab9872ef2aa48b8bdef0163
vagrant <vagrant@homestead>
2018-03-28 04:02:48 +0000

    Style welcome page

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,97 +1,67 @@
 <!doctype html>
-<html lang="{{ app()->getLocale() }}">
+<html class="overflow-y-scroll h-full" lang="{{ app()->getLocale() }}">
     <head>
         <meta charset="utf-8">
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
         <meta name="viewport" content="width=device-width, initial-scale=1">
 
-        <title>Laravel</title>
+        <title>Smultron - Skapa en hemsida för ditt event på bara några sekunder.</title>
+
+        <!-- Tailwind css framework -->
+
+        <link href="https://cdn.jsdelivr.net/npm/tailwindcss/dist/tailwind.min.css" rel="stylesheet">
 
         <!-- Fonts -->
-        <link href="https://fonts.googleapis.com/css?family=Raleway:100,600" rel="stylesheet" type="text/css">
+        <link href="https://fonts.googleapis.com/css?family=Poppins:100,100i,200,200i,300,300i,400,400i,500,500i,600,600i,700,700i,800,800i,900,900i" rel="stylesheet">
 
         <!-- Styles -->
         <style>
-            html, body {
-                background-color: #fff;
-                color: #636b6f;
-                font-family: 'Raleway', sans-serif;
-                font-weight: 100;
-                height: 100vh;
-                margin: 0;
+            .font-sans {
+                font-family: 'Poppins', sans-serif;
             }
-
-            .full-height {
-                height: 100vh;
+            .py-50px {
+                padding-top: 50px;
+                padding-bottom: 50px;
             }
-
-            .flex-center {
-                align-items: center;
-                display: flex;
-                justify-content: center;
-            }
-
-            .position-ref {
-                position: relative;
-            }
-
-            .top-right {
-                position: absolute;
-                right: 10px;
-                top: 18px;
-            }
-
-            .content {
-                text-align: center;
-            }
-
-            .title {
-                font-size: 84px;
-            }
-
-            .links > a {
-                color: #636b6f;
-                padding: 0 25px;
-                font-size: 12px;
-                font-weight: 600;
-                letter-spacing: .1rem;
-                text-decoration: none;
-                text-transform: uppercase;
-            }
-
-            .m-b-md {
-                margin-bottom: 30px;
+            button:focus {
+                outline:0;
             }
         </style>
     </head>
-    <body>
-        <div class="flex-center position-ref full-height">
-            @if (Route::has('login'))
-                <div class="top-right links">
-                    @auth
-                        <a href="{{ url('/home') }}">Home</a>
-                    @else
-                        <a href="{{ route('login') }}">Login</a>
-                        <a href="{{ route('register') }}">Register</a>
-                    @endauth
-                </div>
-            @endif
-
-            <div class="content">
-                <div class="title m-b-md">
-                    Smultronnnnnn
-                </div>
-
-                <p>Förvandla din facebooksida till en egen hemsida på bara några sekunder.</p>
-
-                <div class="links">
-                    <a href="https://laravel.com/docs">Documentation</a>
-                    <a href="https://laracasts.com">Laracasts</a>
-                    <a href="https://laravel-news.com">News</a>
-                    <a href="https://forge.laravel.com">Forge</a>
-                    <a href="https://github.com/laravel/laravel">GitHub</a>
+    <body class="bg-pink-dark font-sans antialiased h-full text-white">
+        <header class="absolute pin-t w-full">
+            <div class="container mx-auto px-4 py-50px flex items-center justify-between">
+                <a href="/" class="flex items-center text-white no-underline">
+                    <img class="w-6" src="{!! asset('storage/logo.png'); !!}" alt="logo" />
+                    <span class="text-base tracking-tight font-bold ml-2">
+                        smultron
+                    </span>
+                </a>
+                <div class="flex items-center">
+                    <a class="bg-white py-2 px-4 rounded-full text-sm font-bold leading-normal text-pink-dark no-underline ml-8 uppercase" href="#">Beta</a>
                 </div>
             </div>
-        </div>
+        </header>
+
+        <section class="bg-pink-dark h-full py-8">
+            <div class="container mx-auto h-full px-4">
+
+                <div class="flex items-center justify-start h-full">
+
+                    <div>
+                        <h1 class="text-4xl xl:text-5xl lg:text-5xl md:text-5xl font-bold leading-tight tracking-tight mb-8">Skapa en hemsida för ditt event på bara några sekunder.</h1>
+                        <p class="w-full xl:w-1/2 lg:w-1/2 md:w-1/2 text-base font-medium leading-normal mb-8">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque molestie justo lacus, a bibendum felis scelerisque sit amet. Morbi vitae nunc varius, faucibus leo quis, gravida risus.</p>
+                        
+                        <div class="flex flex-row sm:flex-row justify-start pt-8">
+                            <button class="mt-8 bg-white text-sm leading-none text-pink-dark font-bold uppercase h-16 px-8 rounded-full whitespace-no-wrap mb-2 sm:mb-0 sm:mr-2">
+                                Skapa en hemida för ditt event
+                            </button>
+                        </div>
+                    </div>
+
+                </div>
+
+            </div>
+        </section>
     </body>
 </html>
